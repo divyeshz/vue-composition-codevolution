131d13c0008048353aa491e80210e7660f6dd5fd
Divyesh Mepal <divyeshm@zignuts.com>
2024-02-19 18:07:47 +0530

    Replacing data with reactive

--- src/components/Data.vue
+++ src/components/Data.vue
@@ -1,30 +1,57 @@
 <template>
     <div>
         <h2>Options - {{ o_firstName }}</h2>
+        <h2>Options - {{ o_firstName }} {{ o_lastName }} a.k.a {{ o_heroName }}</h2>
+
         <h2>Composition - {{ c_firstName }}</h2>
         <h2>Composition - {{ greet }}</h2>
+
+        <h2>Composition - {{ c_firstName }} {{ c_lastName }} a.k.a {{ c_heroName }}</h2>
+        <h2>Composition - {{ greetHero }}</h2>
+
+        <h2>Composition - {{ state.firstName }} {{ state.lastName }} {{ state.heroName }}</h2>
+        <h2>Composition - {{ reactiveGreetHero }}</h2>
+
     </div>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
 
 export default {
     name: 'DataComponent',
     setup() {
-        const c_firstName = ref('Mepal')
-        c_firstName.value = 'Patel'
+        const c_firstName = ref('Neel')
+        const c_lastName = ref('Joshi')
+        const c_heroName = ref('Raju')
+
         const greet = `Hello ${c_firstName.value}`
-        console.log('c_firstName', c_firstName);
+        const greetHero = `Hello ${c_firstName.value} ${c_lastName.value} a.k.a ${c_heroName.value}`
+
+        const state = reactive({
+            firstName: 'Vihang',
+            lastName: 'Patel',
+            heroName: 'Shyam',
+        })
+
+        const reactiveGreetHero = `Hello ${state.firstName} ${state.lastName} a.k.a ${state.heroName}`
 
         return {
             c_firstName,
-            greet
+            c_lastName,
+            c_heroName,
+            greet,
+            greetHero,
+            state,
+            reactiveGreetHero
         }
     },
     data() {
         return {
             o_firstName: 'Divyesh',
+            o_lastName: 'Mepal',
+            o_heroName: 'Dino James',
+
         }
     },
 }
