7179948bd52a6ee9476a11056a198f715657b1a6
Divyesh Mepal <divyeshm@zignuts.com>
2024-02-19 16:54:18 +0530

    Create new vue 3 project for composition api and add code for Replacing data with ref

---
+++ src/components/Data.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h2>Options - {{ o_firstName }}</h2>
+        <h2>Composition - {{ c_firstName }}</h2>
+        <h2>Composition - {{ greet }}</h2>
+    </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+    name: 'DataComponent',
+    setup() {
+        const c_firstName = ref('Mepal')
+        c_firstName.value = 'Patel'
+        const greet = `Hello ${c_firstName.value}`
+        console.log('c_firstName', c_firstName);
+
+        return {
+            c_firstName,
+            greet
+        }
+    },
+    data() {
+        return {
+            o_firstName: 'Divyesh',
+        }
+    },
+}
+</script>
+
+<style scoped></style>
