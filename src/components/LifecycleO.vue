4f9dfedd3795ecd729bea4fa06d47546e3b9c4d7
Divyesh Mepal <divyeshm@zignuts.com>
2024-02-20 18:29:12 +0530

    Replacing Lifecycle Hooks

---
+++ src/components/LifecycleO.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'LifecycleO',
+    beforeCreate() {
+        console.log('LifecycleO beforeCreate()');
+    },
+    created() {
+        console.log('LifecycleO created()');
+    },
+    beforeMount() {
+        console.log('LifecycleO beforeMount()');
+    },
+    mounted() {
+        console.log('LifecycleO mounted()');
+    },
+    beforeUpdate() {
+        console.log('LifecycleO beforeUpdate()');
+    },
+    updated() {
+        console.log('LifecycleO updated()');
+    },
+    beforeUnmount() {
+        console.log('LifecycleO beforeUnmount()');
+    },
+    unmounted() {
+        console.log('LifecycleO unmounted()');
+    },
+}
+</script>
+
+<style scoped></style>
